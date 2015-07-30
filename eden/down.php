19a0ec73698f12c36af7971adf41012594f42f75
Tomas Gal <tomas.gal@upcmail.sk>
2015-07-30 16:18:23 +0200

    eden

---
+++ eden/down.php
@@ -0,0 +1,21 @@
+﻿<?php
+function ping($host)
+{
+  exec(sprintf('ping -c 1 -W 5 %s', escapeshellarg($host)), $res, $rval);
+  return $rval === 0;
+}
+$host = '192.168.1.3';
+$up = ping($host);
+$page = $_SERVER['PHP_SELF'];
+$sec = "10";
+
+echo "Downloader: ";
+echo '<img src="'.($up ? 'on' : 'off').'.jpg" alt="'.($up ? 'UP' : 'NO').'" />';
+
+?>
+
+<html><head>
+    <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
+ </head>
+ <body></body>
+</html>
---
+++ eden/wdc.php
@@ -0,0 +1,21 @@
+﻿<?php
+function ping($host)
+{
+  exec(sprintf('ping -c 1 -W 5 %s', escapeshellarg($host)), $res, $rval);
+  return $rval === 0;
+}
+$host = '192.168.1.5';
+$up = ping($host);
+$page = $_SERVER['PHP_SELF'];
+$sec = "10";
+
+echo "WD Cloud: ";
+echo '<img src="'.($up ? 'on' : 'off').'.jpg" alt="'.($up ? 'UP' : 'NO').'" />';
+
+?>
+
+<html><head>
+    <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
+ </head>
+ <body></body>
+</html>
