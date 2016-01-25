c7be9afee3fbe1bf5e2ef4985f01afb3fb7249d6
Tomas Gal <tomas.gal@upcmail.sk>
2016-01-25 23:34:02 +0100

    System info script update

--- www/si.php
+++ www/si.php
@@ -1,20 +1,8 @@
 <pre>
-<strong>Uptime:</strong>
-<?php system("uptime"); ?>
-<br />
-<strong>System Information:</strong>
-<?php system("uname -a"); ?>
-<br />
-<strong>Memory Usage (MB):</strong>
-<?php system("free -m"); ?>
-<br />
-<strong>Disk Usage:</strong>
-<?php system("df -h"); ?>
-<br />
-<strong>Disk Speed:</strong>
-<?php system("cat ds.txt"); ?>
-<br />
-<strong>CPU Information:</strong>
-<?php system("lshw -class processor "); ?>
+<strong>System Information: </strong><br /><?php system("inxi   -! 31 -C -D -f -I -S -s -c0"); ?><br />
+<strong>Memory Usage (MB):</strong><br /><?php system("free -mht"); ?><br />
+<strong>Disk Usage:</strong><br /><?php system("df -Th"); ?><br />
+<strong>Disk Speed:</strong><?php system("cat ds.txt"); ?><br />
+<strong><?php system("inxi -xxx -w -c0"); ?><br /></strong>
 </pre>
 
