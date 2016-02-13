afab34cd2da8cfae8df6db0d8107d9168f20207a
Tomas Gal <tomas.gal@upcmail.sk>
2016-02-13 18:24:19 +0100

    Changes to be committed: 	modified:   si.php

--- www/si.php
+++ www/si.php
@@ -3,6 +3,6 @@
 <strong>Memory Usage (MB):</strong><br /><?php system("free -mht"); ?><br />
 <strong>Disk Usage:</strong><br /><?php system("df -Th"); ?><br />
 <strong>Disk Speed:</strong><?php system("cat ds.txt"); ?><br />
-<strong><?php system("inxi -xxx -w -c0"); ?><br /></strong>
+<?php system("inxi -xxxW 48.1462538,17.1130809 -c0"); ?><br />
 </pre>
 
